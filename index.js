1c819710117ef03e0529cc29af1055961c4d5240
FrancoMCuello <franco.m.cuello@gmail.com>
2024-06-11 12:15:11 -0300

    Se creo la carpeta routes, se adapto y se creo un pequeño CRUD de productos para prueba

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
+app.use("/productos", require("./routes/productos.routes"));
+
 app.get("/", (req, res) => {
-  res.send("Funciona");
+  res.json("Funciona");
 });
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
