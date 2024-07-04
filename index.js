b36dcb89e85e9ca32755ed806efb170fe3b7c464
FrancoMCuello <franco.m.cuello@gmail.com>
2024-07-04 10:41:57 -0300

    se trabajo con el sistema de auth

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
 app.use("/productos", require("./router/productos.router"));
 app.use("/categorias", require("./router/categorias.router"));
 app.use("/usuarios", require("./router/usuarios.router"));
+app.use("/auth", require("./router/auth.router"));
+const users = require("./models/user.model");
 
 app.get("/", (req, res) => {
   res.json("Bienvenido a nuestra API. GRUPO 11");
